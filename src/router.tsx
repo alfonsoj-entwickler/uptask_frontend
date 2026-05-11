91ecedb5393ac8635c244b6601c0d5e61a0589ad
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2026-05-11 22:29:34 +0200

    add profile routes and update project team route in router

--- src/router.tsx
+++ src/router.tsx
@@ -11,6 +11,11 @@ import ConfirmAccountView from "./views/auth/ConfirmAccountView";
 import RequestNewCodeView from "./views/auth/RequestNewCodeView";
 import ForgotPasswordView from "./views/auth/ForgotPasswordView";
 import NewPasswordView from "./views/auth/NewPasswordView";
+import ProjectTeamView from "./views/projects/ProjectTeamView";
+import ProfileView from "./views/profile/ProfileView";
+import ChangePasswordView from "./views/profile/ChangePasswordView";
+import ProfileLayout from "./layouts/ProfileLayout";
+import NotFound from "./views/404/NotFound";
 
 export default function Router() {
     return (
@@ -30,6 +35,17 @@ export default function Router() {
                         path='/projects/:projectId/edit'
                         element={<EditProjectView />}
                     />
+                    <Route
+                        path='/projects/:projectId/team'
+                        element={<ProjectTeamView />}
+                    />
+                    <Route element={<ProfileLayout />}>
+                        <Route path='/profile' element={<ProfileView />} />
+                        <Route
+                            path='/profile/update-password'
+                            element={<ChangePasswordView />}
+                        />
+                    </Route>
                 </Route>
                 <Route element={<AuthLayout />}>
                     <Route path='/auth/login' element={<LoginView />} />
@@ -50,6 +66,9 @@ export default function Router() {
                         path='/auth/new-password'
                         element={<NewPasswordView />}
                     />
+                </Route>
+                <Route element={<AuthLayout />}>
+                    <Route path='*' element={<NotFound />} />
                 </Route>
             </Routes>
         </BrowserRouter>
